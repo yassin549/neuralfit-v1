992be16ce9262cf2e4d43e1eab129ef6c976e59e
yassin549 <khoualdiyassin26@gmail.com>
2025-08-03 09:00:56 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+  ignorePatterns: ['node_modules', 'dist', 'build'],
+};
