94bf399f347292ae984c79dcf36b3ef504be9a3d
yassin549 <khoualdiyassin26@gmail.com>
2025-08-29 20:42:38 +0100

    first commit

--- infra/terraform/modules/redis/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "redis_primary_endpoint" {
-  description = "The primary endpoint of the Redis cluster"
-  value       = aws_elasticache_cluster.redis.cache_nodes[0].address
-}
-
-output "redis_port" {
-  description = "The port of the Redis cluster"
-  value       = aws_elasticache_cluster.redis.port
-}
